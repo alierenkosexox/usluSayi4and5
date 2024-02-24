d27948cc24ec04d36935d7cbbdf843d3ad7e9b99
Ali Eren KÖSE <alierenkose@gmail.com>
2024-02-24 17:51:45 +0300

    xyz

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int number;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sınır Sayısını Giriniz : ");        //sınır sayısı sor ve number'e ata
+        number = input.nextInt();
+
+        System.out.println("4'ün kuvvetleri");
+        System.out.println("------------------------");
+        for (int i = 1; Math.pow(4, i) <= number; i++) { //sınır sayısını geçmediği taktirde üssü bir arttır ve işleme devam et
+            System.out.println(Math.pow(4, i));
+        }
+        System.out.println("------------------------");
+        System.out.println("5'in kuvvetleri");
+        for (int i = 1; Math.pow(5, i) <= number; i++) { ////sınır sayısını geçmediği taktirde üssü bir arttır ve işleme devam et
+            System.out.println(Math.pow(5, i));
+
         }
     }
-}
+} //Ali Eren KÖSE 24/02/2024 17:48
